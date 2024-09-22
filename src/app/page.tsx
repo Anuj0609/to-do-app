e97017bb7a35742f1925db25cb44d5724951d34e
Anuj <anujshrivastava982@gmail.com>
2024-09-22 12:33:12 +0530

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,108 @@
-import Image from "next/image";
+"use client";
+import React, { useState, useEffect } from "react";
+
+interface Task {
+  id: number;
+  name: string;
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [inputTask, setInputTask] = useState("");
+  const [taskList, setTaskList] = useState<Task[]>([]);
+  const [isEditable, setIsEditable] = useState(false);
+  const [currentTask, setCurrentTask] = useState<Task | null>(null);
+  const [newTaskName, setNewTaskName] = useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const addTask = () => {
+    if (inputTask.trim()) {
+      const newTask: Task = {
+        name: inputTask,
+        id: Math.floor(Math.random() * 99999),
+      };
+      const updatedTask = [...taskList, newTask];
+      localStorage.setItem("taskLists", JSON.stringify(updatedTask));
+      setTaskList(updatedTask);
+      setInputTask("");
+    }
+  };
+
+  const getTask = () => {
+    const list = localStorage.getItem("taskLists");
+    if (list) {
+      setTaskList(JSON.parse(list));
+    } else setTaskList([]);
+  };
+
+  useEffect(() => {
+    getTask();
+  }, []);
+
+  const clearTask = () => {
+    setTaskList([]);
+    localStorage.removeItem("taskLists");
+  };
+
+  const deleteTask = (id: number) => {
+    const updatedTask = taskList.filter((task: Task) => task.id !== id);
+    localStorage.setItem("taskLists", JSON.stringify(updatedTask));
+    setTaskList(updatedTask);
+  };
+
+  const enableEditing = (task: Task) => {
+    setIsEditable(true);
+    setCurrentTask(task);
+    setNewTaskName(task.name);
+  };
+
+  const updateTask = () => {
+    const updatedTask = taskList.map((task: Task) =>
+      task.id === currentTask!.id ? { ...task, name: newTaskName } : task
+    );
+    localStorage.setItem("taskLists", JSON.stringify(updatedTask));
+    setTaskList(updatedTask);
+    setIsEditable(false);
+    setCurrentTask(null);
+    setNewTaskName("");
+  };
+
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col items-center">
+        <h2>TO-Do-LIST</h2>
+        <div>
+          <input
+            type="text"
+            placeholder="Add your Task"
+            value={inputTask}
+            onChange={(e) => setInputTask(e.target.value)}
+          />
+          <button onClick={addTask}>Add Task</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <ul>
+          {taskList.map((task: Task) => (
+            <li key={task.id}>
+              {isEditable && currentTask && currentTask.id === task.id ? (
+                <input
+                  type="text"
+                  value={newTaskName}
+                  onChange={(e) => setNewTaskName(e.target.value)}
+                />
+              ) : (
+                <h5>{task.name}</h5>
+              )}
+              <div>
+                {isEditable && currentTask && currentTask.id === task.id ? (
+                  <button onClick={updateTask}>Save</button>
+                ) : (
+                  <button onClick={() => enableEditing(task)}>Edit</button>
+                )}
+                <button onClick={() => deleteTask(task.id)}>deleteTask</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <button onClick={clearTask}>Clear All</button>
+      </div>
     </div>
   );
 }
