53428cd4ce99c881cc1c6313239de3c96396035b
Anuj <anujshrivastava982@gmail.com>
2024-12-21 19:12:01 +0530

    fix build

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
     <div className="flex items-center justify-center flex-col md:pt-20 pt-10  gap-4 px-4 sm:px-0">
       <img src="./logo.png" alt="App Logo" className="w-32 sm:w-auto" />
       <div className="text-center text-2xl font-semibold text-gray-700">
-        "Stay on top of tasks, effortlessly."
+        Stay on top of tasks, effortlessly
       </div>
     </div>
   );
